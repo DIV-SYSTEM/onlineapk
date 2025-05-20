cd12343b1481958ad3e7e10413c0e9773edb8dbf
Divyanshu T <tdivyanshu028@gmail.com>
2025-05-20 13:02:22 -0700

    Update containgd.dart

--- lib/Govt_Defense/containgd.dart
+++ lib/Govt_Defense/containgd.dart
@@ -8,47 +8,37 @@ import 'gd6.dart';
 import 'gd7.dart';
 import 'gd8.dart';
 import 'gd9.dart';
-import "gd10.dart";
-import "gd11.dart";
-import "gd12.dart";
-import "gd13.dart";
-import "gd14.dart";
+import 'gd10.dart';
+import 'gd11.dart';
+import 'gd12.dart';
+import 'gd13.dart';
+import 'gd14.dart';
+import '../Screen/screen.dart'; // Import GenericScreen from Screen folder
+
 class ContainGD extends StatelessWidget {
   const ContainGD({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width < 700 ? double.infinity : 600,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                AM1(),
-                AM2Section(),
-                AM3(),
-                Am4(),
-                AM5(),
-                Am6(),
-                Am7(),
-                AM8(),
-
-                AmTcSection(),
-                Cloud4(),
-                Cloud5(),
-                ContactInfoSection(),
-
-
-
-              ],
-            ),
-          ),
-        ),
-      ),
+    return GenericScreen(
+      title: 'Government and Defense Services', // Set the title for the AppBar
+      maxWidth: 600.0, // Match ContainGD's maxWidth
+      contentSections: const [
+        AM1(),
+        AM2Section(),
+        AM3(),
+        Am4(),
+        AM5(),
+        Am6(),
+        Am7(),
+        AM8(),
+        AM9(),
+        AmTcSection(),
+        Cloud4(),
+        Cloud5(),
+        ContactInfoSection(),
+        ContactForm(),
+      ],
     );
   }
 }
