cbad90fe5a331a9883038b3ddce1a3ad2d92058c
Divyanshu T <tdivyanshu028@gmail.com>
2025-05-20 12:50:44 -0700

    Update containam.dart

--- lib/Automobile/containam.dart
+++ lib/Automobile/containam.dart
@@ -8,49 +8,38 @@ import 'am6.dart';
 import 'am7.dart';
 import 'am8.dart';
 import 'am9.dart';
-import "am10.dart";
-import "a11.dart";
-//import "am12.dart";
-import "amtc.dart";
-import "am14.dart";
-
-import 'package:flutter_app/Service/cloud6.dart';
+import 'am10.dart';
+import 'a11.dart';
+//import 'am12.dart';
+import 'amtc.dart';
+import 'am14.dart';
+import '../Service/cloud6.dart';
+import '../Screen/screen.dart'; // Import GenericScreen from Screen folder
 
 class ContainAM extends StatelessWidget {
   const ContainAM({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width < 700 ? double.infinity : 600,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                AM1(),
-                AM2Section(),
-                AM3(),
-                Am4(),
-                AM5(),
-                Am6(),
-                Am7(),
-                AM8(),
-                AM9(),
-                AmTcSection(),
-                Cloud4(),
-                Cloud5(),
-                Cloud6(),
-                ContactForm(),
-
-              ],
-            ),
-          ),
-        ),
-      ),
+    return GenericScreen(
+      title: 'Automotive Services', // Set the title for the AppBar
+      maxWidth: 600.0, // Match ContainAM's maxWidth
+      contentSections: const [
+        AM1(),
+        AM2Section(),
+        AM3(),
+        Am4(),
+        AM5(),
+        Am6(),
+        Am7(),
+        AM8(),
+        AM9(),
+        AmTcSection(),
+        Cloud4(),
+        Cloud5(),
+        Cloud6(),
+        ContactForm(),
+      ],
     );
   }
 }
