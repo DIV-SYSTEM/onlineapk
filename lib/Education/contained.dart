53823b9b5145946709fd7804b6ea07185e9a7d97
Divyanshu T <tdivyanshu028@gmail.com>
2025-05-20 12:56:21 -0700

    Update contained.dart

--- lib/Education/contained.dart
+++ lib/Education/contained.dart
@@ -8,46 +8,38 @@ import 'ed6.dart';
 import 'ed7.dart';
 import 'ed8.dart';
 import 'ed9.dart';
-import "ed10.dart";
-import "ed11.dart";
-import "ed12.dart";
-import "ed13.dart";
-import "ed14.dart";
+import 'ed10.dart';
+import 'ed11.dart';
+//import 'ed12.dart';
+import 'ed13.dart';
+import 'ed14.dart';
+import '../Service/cloud6.dart';
+import '../Screen/screen.dart'; // Import GenericScreen from Screen folder
+
 class ContainED extends StatelessWidget {
   const ContainED({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width < 700 ? double.infinity : 600,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                AM1(),
-                AM2Section(),
-                AM3(),
-                Am4(),
-                AM5(),
-                Am6(),
-                Am7(),
-                AM8(),
-                AM9(),
-                AmTcSection(),
-                Cloud4(),
-                Cloud5(),
-                Cloud6(),
-                Cloud7(),
-
-              ],
-            ),
-          ),
-        ),
-      ),
+    return GenericScreen(
+      title: 'Edu-Tech Services', // Set the title for the AppBar
+      maxWidth: 600.0, // Match ContainED's maxWidth
+      contentSections: const [
+        AM1(),
+        AM2Section(),
+        AM3(),
+        Am4(),
+        AM5(),
+        Am6(),
+        Am7(),
+        AM8(),
+        AM9(),
+        AmTcSection(),
+        Cloud4(),
+        Cloud5(),
+        Cloud6(),
+        ContactForm(),
+      ],
     );
   }
 }
