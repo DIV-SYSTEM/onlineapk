ab114a5cf90d107228978aad946aa45af3b23230
Divyanshu T <tdivyanshu028@gmail.com>
2025-05-30 19:47:46 -0700

    Update containtl.dart

--- lib/Telecomm/containtl.dart
+++ lib/Telecomm/containtl.dart
@@ -8,46 +8,37 @@ import 'tl6.dart';
 import 'tl7.dart';
 import 'tl8.dart';
 import 'tl9.dart';
-import "tl10.dart";
-import "tl11.dart";
-import "tl12.dart";
-import "tl13.dart";
-import "tl14.dart";
+import 'tl10.dart';
+import 'tl11.dart';
+import 'tl12.dart';
+import 'tl13.dart';
+import 'tl14.dart';
+import '../Screen/screen.dart'; // Import GenericScreen from Screen folder
+
 class ContainTL extends StatelessWidget {
   const ContainTL({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 24),
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width < 700 ? double.infinity : 600,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                AM1(),
-                AM2Section(),
-                AM3(),
-                Am4(),
-                AM5(),
-                Am6(),
-                Am7(),
-                AM8(),
-                AM9(),
-                AmTcSection(),
-                Cloud4(),
-                Cloud5(),
-                Cloud6(),
-                Cloud7(),
-
-              ],
-            ),
-          ),
-        ),
-      ),
+    return GenericScreen(
+      title: 'Telecommunication Services', // Set the title for the AppBar
+      maxWidth: 600.0, // Match ContainTL's maxWidth
+      contentSections: const [
+        AM1(),
+        AM2Section(),
+        AM3(),
+        Am4(),
+        AM5(),
+        Am6(),
+        Am7(),
+        AM8(),
+        //AM9(),
+        AmTcSection(),
+        Telecom4(),
+        Cloud5(),
+        ContactInfoSection(),
+        ContactForm(),
+      ],
     );
   }
 }
